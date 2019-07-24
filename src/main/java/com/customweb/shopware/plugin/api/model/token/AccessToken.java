64691ac690976400ba81af0a343f12c9ce4a7883
Nico Eigenmann <nico.eigenmann@customweb.com>
2019-07-24 09:23:28 +0200

    Added access token legacy property

--- src/main/java/com/customweb/shopware/plugin/api/model/token/AccessToken.java
+++ src/main/java/com/customweb/shopware/plugin/api/model/token/AccessToken.java
@@ -7,25 +7,29 @@ import java.util.Map;
 
 import com.customweb.shopware.plugin.api.util.DateUtil;
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * Access token received from the Shopware system.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AccessToken {
 
 	private final Locale locale;
 	private final String token;
 	private final String userId;
 	private final ZonedDateTime expiryDate;
+	private final Boolean legacyLogin;
 
 	@JsonCreator
 	public AccessToken(@JsonProperty("expire") Map<String, String> expiration, @JsonProperty("locale") Locale locale,
-			@JsonProperty("token") String token, @JsonProperty("userId") String userId) {
+			@JsonProperty("token") String token, @JsonProperty("userId") String userId, @JsonProperty(value = "legacyLogin", required= false) Boolean legacyLogin) {
 		expiryDate = DateUtil.parseDate(expiration.get("date")).atZone(ZoneId.of(expiration.get("timezone")));
 		this.locale = locale;
 		this.token = token;
 		this.userId = userId;
+		this.legacyLogin = legacyLogin;
 	}
 
 	public Locale getLocale() {
@@ -44,4 +48,7 @@ public class AccessToken {
 		return expiryDate;
 	}
 
+	public Boolean getLegacyLogin() {
+		return legacyLogin;
+	}
 }
